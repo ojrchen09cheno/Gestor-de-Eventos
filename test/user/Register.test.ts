a04cbcee1777e4894bc40d868653afbbf5875f0c
Jia Chen <jiamchenchen@gmail.com>
2024-04-04 17:55:35 -0500

    Initial test files

---
+++ test/user/Register.test.ts
@@ -0,0 +1,7 @@
+import { RegisterApp } from "@application/services/user/RegisterApp";
+
+describe("Test para registro de usuario", () => {
+  it("Deberia crear un usuario si no existe", async () => {
+    
+  })
+})
